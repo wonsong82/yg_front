4e60a096d6eab9301f163ff270eca073febb219c
won song <wcsong@gobaesong.com>
2016-09-20 20:02:58 -0400

    Artist page order change

--- app/components/app/page/Artist.js
+++ app/components/app/page/Artist.js
@@ -49,6 +49,44 @@ class Artist extends Component {
         <h1 className="page-title" style={{color:themeColor}}>{name}</h1>
 
 
+        {products && products.length ?
+          <div className="shop-container">
+            <h3>Shop</h3>
+            <ul className="products-list">
+              {products.map( (item, i) => (
+                <ProductGroup key={`products-${i}`} products={item} />
+              ))}
+            </ul>
+            {!productsAllLoaded &&
+            <ViewMore onClick={onProductsViewMoreClick} />
+            }
+          </div>
+          :null}
+
+        {albums && albums.length ?
+          <div className="music-container">
+            <section className="main">
+              <h3>Album</h3>
+              {albums && albums.length ? albums.map( (item, i) => (
+                <AlbumGroup key={`albumGroup-${i}`} albums={item} />
+              )):null}
+              {!albumsAllLoaded && albums && albums.length ?
+                <ViewMore onClick={onAlbumsViewMoreClick} />
+                : ''}
+            </section>
+            <section className="side">
+              <h3>Hot Tracks</h3>
+              {hotTracks && hotTracks.length ? hotTracks.map( (item, i) => (
+                <HotTrackGroup key={`hotTrackGroup-${i}`} hotTracks={item} onAddCartClick={onAddToCartClick}/>
+              )): null}
+              {!hotTracksAllLoaded && hotTracks && hotTracks.length ?
+                <ViewMore onClick={onHotTracksViewMoreClick} />
+                : ''}
+            </section>
+          </div>
+          :null}
+
+
         {tours && tours.length ?
         <div className="tour-container">
           <h3>Tour</h3>
@@ -69,49 +107,12 @@ class Artist extends Component {
             ))}
           </ul>
           {!toursAllLoaded &&
-          <ViewMore onClick={onToursViewMoreClick} />
+          <ViewMore onClick={onToursViewMoreClick} /> 
           }
         </div>
         :null}
 
 
-        {products && products.length ?
-        <div className="shop-container">
-          <h3>Shop</h3>
-          <ul className="products-list">
-            {products.map( (item, i) => (
-              <ProductGroup key={`products-${i}`} products={item} />
-            ))}
-          </ul>
-          {!productsAllLoaded &&
-          <ViewMore onClick={onProductsViewMoreClick} />
-          }
-        </div>
-        :null}
-
-
-        {albums && albums.length ?
-        <div className="music-container">
-          <section className="main">
-            <h3>Album</h3>
-            {albums && albums.length ? albums.map( (item, i) => (
-              <AlbumGroup key={`albumGroup-${i}`} albums={item} />
-            )):null}
-            {!albumsAllLoaded && albums && albums.length ?
-              <ViewMore onClick={onAlbumsViewMoreClick} />
-              : ''}
-          </section>
-          <section className="side">
-            <h3>Hot Tracks</h3>
-            {hotTracks && hotTracks.length ? hotTracks.map( (item, i) => (
-              <HotTrackGroup key={`hotTrackGroup-${i}`} hotTracks={item} onAddCartClick={onAddToCartClick}/>
-            )): null}
-            {!hotTracksAllLoaded && hotTracks && hotTracks.length ?
-              <ViewMore onClick={onHotTracksViewMoreClick} />
-              : ''}
-          </section>
-        </div>
-        :null}
 
 
         {events && events.length ?
